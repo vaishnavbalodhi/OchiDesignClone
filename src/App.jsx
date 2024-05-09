381de6c1394de5b4acff3144be05ebf863466e3b
vaishnavbalodhi <balodhivaishnav@gmail.com>
2024-05-09 22:43:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full h-screen bg-zinc-900 text-white'>
+        <Navbar />
+      </div>
+    </>
+  )
+}
+
+export default App
